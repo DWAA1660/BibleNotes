fde2624c20e26e77e9a71f3fab5e425c18b3e1a6
DWAA1660 <dwatnip123@gmail.com>
2025-10-24 09:05:34 -0600

    profile page fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      "/api": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, "")
-      }
-    }
-  }
-});
+  preview: {
+    host: true,
+    port: 3170,
+    allowedHosts: true,       // allow any host
+  },
+})
